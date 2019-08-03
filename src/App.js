4b3337550297ea023111e0e4ab8c407476509a6a
Rodrigo Carrillo-Sanchez <rocarrillos@hotmail.com>
2019-08-02 22:05:40 -0400

    add bootstrap and create navbar class

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import AppRouter from './AppRouter';
+import MenuBar from './MenuBar';
 
 function App() {
   return (
     <div>
+      <MenuBar/>
       <AppRouter/>
     </div>
   );

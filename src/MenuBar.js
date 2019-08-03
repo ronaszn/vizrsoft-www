06cc1d2e0741403a0897d8f95fcf391f2e5f06e4
Rodrigo Carrillo-Sanchez <rocarrillos@hotmail.com>
2019-08-02 23:48:52 -0400

    add navbar

--- src/MenuBar.js
+++ src/MenuBar.js
@@ -1,17 +1,28 @@
-import React from 'react'
-import Navbar from 'react-bootstrap/Navbar'
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import {Nav, NavItem, NavDropDown, MenuItem} from 'react-bootstrap';
 
 // TODO
-// About Us
-// Locations
-// Contact Us
-// Products
+// About Us /about-us
+// Locations /locations
+// Contact Us /contact-us
+// Products /products
 
 class MenuBar extends React.Component {
     render() {
-        return <Navbar bg='dark' expand='lg'>
-
+        return (
+        <Navbar bg='dark' expand='lg'>
+            <Navbar.Brand href="home">VZRSoft</Navbar.Brand>
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link className="nav-link" href="about-us">About Us</Nav.Link>
+                    <Nav.Link className="nav-link" href="locations">Locations</Nav.Link>
+                    <Nav.Link className="nav-link" href="contact-us">Contact Us</Nav.Link>
+                    <Nav.Link className="nav-link" href="products">Products</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
         </Navbar>
+        )
     }
 }
 

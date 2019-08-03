5b4325379eb573b526001074f7a5588b930f5a54
Eduardo Alvarez <eduardo_e_alvarez@homedepot.com>
2019-08-02 22:49:57 -0400

    added contact us page link

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import HomeLayout from "./HomeLayout";
 import SubPage from './SubPage';
+import ContactUs from './ContactUs';
 
 
 class AppRouter extends Component {
@@ -16,6 +17,7 @@ class AppRouter extends Component {
                 <div>
                     <Route path="/" render={(props)=><HomeLayout {...props}/>}/>
                     <Route exact path="/sub-page" component={SubPage} />
+                    <Route exact path="/sub-page" component={ContactUs} />
                     {/* more Route components are to be added here for different pages */}
                 </div>
             </Router>
